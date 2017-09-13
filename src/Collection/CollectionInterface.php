863155d75209405c2e9afdba82cf9df703df7fb2
Fwolf <fwolf.aide+git@gmail.com>
2017-09-13 23:58:36 +0800

    Add note about key and element type

--- src/Collection/CollectionInterface.php
+++ src/Collection/CollectionInterface.php
@@ -10,6 +10,17 @@ use IteratorAggregate;
 /**
  * Collection is better way to operate array alike data
  *
+ * Collection focus on group and operate on values, not 'key', although inner
+ * property $element is array with auto number key in default, and some method
+ * use key as parameter, the key of element may change in may ways, so do not
+ * rely on them. User may use assoc key or mix assoc key with auto increment key
+ * too.
+ *
+ * Some sort or search method may need compare elements, use them only when you
+ * know what type the elements are and if they are comparable.
+ *
+ * Suggestion: Collection with mixed type elements is terrible, use carefully.
+ *
  * @copyright   Copyright 2017 Fwolf
  * @license     https://opensource.org/licenses/MIT MIT
  */
